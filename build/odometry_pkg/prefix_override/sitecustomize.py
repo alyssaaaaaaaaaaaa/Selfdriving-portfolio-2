83ddb7fc130114af6ddd8304e87e3d19df254d72
alyssaaaaaaaaaaaa <150351918+alyssaaaaaaaaaaaa@users.noreply.github.com>
2026-05-16 12:10:39 +0200

    Delete build directory

--- build/odometry_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/alyssa/Documenten/GitHub/Selfdriving-portfolio-2/install/odometry_pkg'
